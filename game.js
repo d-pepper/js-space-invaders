4385ae8f3d05b8a5849e7ff376d8d9d9c9b4e91f
dale.pepper <dale.pepper@tungsten-network.com>
2015-07-14 13:59:56 +0100

    Initial commit

---
+++ game.js
@@ -0,0 +1,16 @@
+;(function() {
+	var Game = function(canvasId){
+		var canvas = document.getElementById(canvasId);
+		var screen = canvas.getContext('2d');
+		var gameSize = { x: canvas.width, y: canvas.height };
+		console.log("hi");
+	};
+	
+	Game.prototype = {
+		
+	};
+	
+	window.onload = function() {
+		new Game("screen");
+	};
+})();
